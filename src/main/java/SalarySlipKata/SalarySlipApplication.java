d4bc6979959d351e8fd27ac4f0d9a774c709415a
Mani Sarkar <mani@codurance.com>
2016-09-09 12:20:55 +0100

    Run the SalarySlipApplication implementation, this forces us to implement Console which has been used as a mock all this while

--- src/main/java/SalarySlipKata/SalarySlipApplication.java
+++ src/main/java/SalarySlipKata/SalarySlipApplication.java
@@ -2,6 +2,8 @@ package SalarySlipKata;
 
 import SalarySlipKata.domain.Employee;
 import SalarySlipKata.domain.EmployeeId;
+import SalarySlipKata.infrastructure.Clock;
+import SalarySlipKata.infrastructure.Console;
 import SalarySlipKata.infrastructure.InMemoryEmployeeRepository;
 import SalarySlipKata.infrastructure.SalarySlipPrinter;
 
@@ -9,6 +11,19 @@ public class SalarySlipApplication {
 
   private SalarySlipPrinter salarySlipPrinter;
   private InMemoryEmployeeRepository inMemoryEmployeeRepository;
+
+  public static void main(String[] args) {
+    final EmployeeId employeeId = new EmployeeId(12345);
+    InMemoryEmployeeRepository inMemoryEmployeeRepository = new InMemoryEmployeeRepository();
+    inMemoryEmployeeRepository.add(employeeId, new Employee(employeeId, "John J Doe", 24000));
+
+    Clock clock = new Clock();
+    Console console = new Console();
+    SalarySlipPrinter salarySlipPrinter = new SalarySlipPrinter(clock, console);
+    final SalarySlipApplication salarySlipApplication = new SalarySlipApplication(inMemoryEmployeeRepository, salarySlipPrinter);
+
+    salarySlipApplication.printFor(employeeId, "Sep 2016");
+  }
 
   public SalarySlipApplication(
       InMemoryEmployeeRepository inMemoryEmployeeRepository,
--- src/main/java/SalarySlipKata/infrastructure/Console.java
+++ src/main/java/SalarySlipKata/infrastructure/Console.java
@@ -2,6 +2,6 @@ package SalarySlipKata.infrastructure;
 
 public class Console {
   public void print(String text) {
-    throw new UnsupportedOperationException();
+    System.out.println(text);
   }
 }
