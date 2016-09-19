40a5b68647f0422cd05f608db44994c5d1c5b5d5
Mani Sarkar <mani@codurance.com>
2016-09-19 17:25:27 +0100

    Refactored the NationalInsuranceService by extracting constants and giving it meaningful names. Also extracted a number of blocks of code into their respective methods and refactored to composed methods.

--- src/main/java/SalarySlipKata/application_service/NationalInsuranceService.java
+++ src/main/java/SalarySlipKata/application_service/NationalInsuranceService.java
@@ -3,20 +3,37 @@ package SalarySlipKata.application_service;
 public class NationalInsuranceService {
 
   private static final double NO_CONTRIBUTIONS_ALLOWANCE = 8060.00;
+
+  private static final double BASIC_CONTRIBUTIONS_LOWER_LIMIT = 8060.00;
+  private static final double BASIC_CONTRIBUTIONS_HIGHER_LIMIT = 43000.00;
+  private static final double BASIC_CONTRIBUTIONS_DIFFERENCE =
+      BASIC_CONTRIBUTIONS_HIGHER_LIMIT - BASIC_CONTRIBUTIONS_LOWER_LIMIT;
   private static final double BASIC_CONTRIBUTIONS_RATE = 0.12;
 
-  public double calculateFor(double annualSalary) {
-    double deductibleSalary = annualSalary - NO_CONTRIBUTIONS_ALLOWANCE;
+  private static final double HIGHER_CONTRIBUTIONS_LIMIT = 43000.00;
+  private static final double HIGHER_CONTRIBUTIONS_RATE = 0.02;
 
-    if (annualSalary > 43000.00) {
-      double excessOver43000 = annualSalary - 43000.00;
-      return ((43000 - NO_CONTRIBUTIONS_ALLOWANCE) * BASIC_CONTRIBUTIONS_RATE) + (excessOver43000 * 0.02);
+  public double calculateFor(double annualSalary) {
+    if (greaterThanHigherContributionsLimit(annualSalary)) {
+      return calculateBasicContributions(BASIC_CONTRIBUTIONS_DIFFERENCE) +
+          getHigherContributionsLimit(annualSalary);
     }
 
+    double deductibleSalary = getDeductibleSalary(annualSalary);
     if (deductibleSalary > 0.0) {
-      return deductibleSalary * BASIC_CONTRIBUTIONS_RATE;
+      return calculateBasicContributions(deductibleSalary);
     }
 
     return 0.0;
   }
+
+  private boolean greaterThanHigherContributionsLimit(double annualSalary) {return annualSalary > HIGHER_CONTRIBUTIONS_LIMIT;}
+
+  private double getDeductibleSalary(double annualSalary) {return annualSalary - NO_CONTRIBUTIONS_ALLOWANCE;}
+
+  private double getHigherContributionsLimit(double annualSalary) {
+    return (annualSalary - HIGHER_CONTRIBUTIONS_LIMIT) * HIGHER_CONTRIBUTIONS_RATE;
+  }
+
+  private double calculateBasicContributions(double basicContributionDifference) {return basicContributionDifference * BASIC_CONTRIBUTIONS_RATE;}
 }
