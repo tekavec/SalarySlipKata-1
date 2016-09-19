2d50e2a7e7e76c54d34f69a3663ac282935ae41c
Mani Sarkar <mani@codurance.com>
2016-09-19 18:46:17 +0100

    - Implemented the TaxService as per the tax bands table including edge cases. - Refactored the TaxService and SalaryService: extracted constants and methods for the respective salary bands based on the tax calculation table

--- src/main/java/SalarySlipKata/application_service/TaxService.java
+++ src/main/java/SalarySlipKata/application_service/TaxService.java
@@ -1,14 +1,26 @@
 package SalarySlipKata.application_service;
 
 public class TaxService {
-  private static final double PERSONAL_ALLOWANCE = 11000.00;
+  private static final double PERSONAL_ALLOWANCE = 11_000.00;
+  private static final double BASIC_RATE_LOWER_LIMIT = 11_000.00;
+  private static final double BASIC_RATE_UPPER_LIMIT = 43_000.00;
+  private static final double
+      BASIC_RATE_LIMITS_DIFFERENCE = BASIC_RATE_UPPER_LIMIT - BASIC_RATE_LOWER_LIMIT;
   private static final double BASIC_RATE = 0.20;
 
+  private static final double PERSONAL_ALLOWANCE_REDUCTION_RULE_SALARY_THRESHOLD = 100_000.00;
+
+  private static final double HIGHER_RATE_UPPER_LIMIT = 150_000.00;
+  private static final double HIGHER_RATE = 0.40;
+
+  private static final double ADDITIONAL_RATE_LOWER_LIMIT = 150_000.00;
+  private static final double ADDITIONAL_RATE = 0.45;
+
   public double getTaxFreeAllowanceFor(double annualSalary) {
-    if (annualSalary > 100000) {
-      double difference = annualSalary - 100000;
+    if (annualSalary > PERSONAL_ALLOWANCE_REDUCTION_RULE_SALARY_THRESHOLD) {
+      double difference = annualSalary - PERSONAL_ALLOWANCE_REDUCTION_RULE_SALARY_THRESHOLD;
       double halfDifference = difference / 2;
-      return PERSONAL_ALLOWANCE >= halfDifference
+      return PERSONAL_ALLOWANCE > halfDifference
                 ? PERSONAL_ALLOWANCE - halfDifference
                 : 0.0;
     }
@@ -29,22 +41,30 @@ public class TaxService {
       return 0.0;
     }
 
-    if (annualSalary > 150000.00) {
-      return (annualSalary - 150000.00) * 0.45 +
-          ((150000 - 32000.00) * 0.40) +
-          ((43000.00 - 11000.00) * BASIC_RATE);
+    if (annualSalary > ADDITIONAL_RATE_LOWER_LIMIT) {
+      return getAdditionalRateTax(annualSalary)
+          + getHigherRateTaxWhen100KRuleApplies(HIGHER_RATE_UPPER_LIMIT)
+          + getBasicRateTax();
     }
 
-    if (annualSalary > 100000.00) {
-      return ((taxableIncome - 32000.00) * 0.40) +
-          ((43000.00 - 11000.00) * BASIC_RATE);
+    if (annualSalary > PERSONAL_ALLOWANCE_REDUCTION_RULE_SALARY_THRESHOLD) {
+      return getHigherRateTaxWhen100KRuleApplies(taxableIncome)
+          + getBasicRateTax();
     }
 
-    if (annualSalary > 43000.00) {
-      return ((annualSalary - 43000.00) * 0.40) +
-          ((43000.00 - 11000.00) * BASIC_RATE);
+    if (annualSalary > BASIC_RATE_UPPER_LIMIT) {
+      return getHigherRateTaxOnRemainingSalary(annualSalary)
+          + getBasicRateTax();
     }
 
     return taxableIncome * BASIC_RATE;
   }
+
+  protected double getAdditionalRateTax(double annualSalary) {return (annualSalary - ADDITIONAL_RATE_LOWER_LIMIT) * ADDITIONAL_RATE;}
+
+  protected double getHigherRateTaxWhen100KRuleApplies(double taxableIncome) {return (taxableIncome - BASIC_RATE_LIMITS_DIFFERENCE) * HIGHER_RATE;}
+
+  private double getHigherRateTaxOnRemainingSalary(double annualSalary) {return (annualSalary - BASIC_RATE_UPPER_LIMIT) * HIGHER_RATE;}
+
+  private double getBasicRateTax() {return BASIC_RATE_LIMITS_DIFFERENCE * BASIC_RATE;}
 }
