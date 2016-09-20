f837b521dc4c6563ca8d3aefffeb504d2de004af
Mani Sarkar <mani@codurance.com>
2016-09-20 15:30:01 +0100

    Extracted methods from the salary calculations

--- src/main/java/SalarySlipKata/SalarySlipApplication.java
+++ src/main/java/SalarySlipKata/SalarySlipApplication.java
@@ -11,26 +11,30 @@ import SalarySlipKata.domain.Employee;
 public class SalarySlipApplication {
 
   public List<String> generateFor(Employee employee) {
+
     List<String> salarySlip = new ArrayList<>();
 
     salarySlip.add(format("Employee ID: %s%n", valueOf(employee.id())));
     salarySlip.add(format("Employee Name: %s%n", employee.name()));
-
-    final int annualSalary = employee.annualSalary();
-    salarySlip.add(format("Gross Salary: %s%n", formatAmount(perMonth(annualSalary))));
-    double taxFreeAllowance = 11_000.00;
-    salarySlip.add(format("Tax-free allowance: %s%n", formatAmount(perMonth(taxFreeAllowance))));
-    double taxableIncome = annualSalary - taxFreeAllowance;
-    salarySlip.add(format("Taxable income: %s%n", formatAmount(perMonth(taxableIncome))));
-    double nationInsuranceContributions = (annualSalary - 8060) * 0.12;
-    salarySlip.add(format("National Insurance contributions: %s%n", formatAmount(perMonth(nationInsuranceContributions))));
-    double taxPayable = (annualSalary - taxFreeAllowance) * 0.20;
-    salarySlip.add(format("Tax Payable: %s%n", formatAmount(perMonth(taxPayable))));
-    double netPayable = annualSalary - (nationInsuranceContributions + taxPayable);
-    salarySlip.add(format("Net Payable: %s%n", formatAmount(perMonth(netPayable))));
+    salarySlip.add(format("Gross Salary: %s%n", formatAmount(perMonth(employee.annualSalary()))));
+    salarySlip.add(format("Tax-free allowance: %s%n", formatAmount(perMonth(getTaxFreeAllowance()))));
+    salarySlip.add(format("Taxable income: %s%n", formatAmount(perMonth(getTaxableIncome(employee.annualSalary())))));
+    salarySlip.add(format("National Insurance contributions: %s%n", formatAmount(perMonth(getNationInsuranceContributions(employee.annualSalary())))));
+    salarySlip.add(format("Tax Payable: %s%n", formatAmount(perMonth(getTaxPayable(employee.annualSalary())))));
+    salarySlip.add(format("Net Payable: %s%n", formatAmount(perMonth(getNetPayable(employee.annualSalary())))));
 
     return salarySlip;
   }
+
+  private double getNationInsuranceContributions(double annualSalary) {return (annualSalary - 8060) * 0.12;}
+
+  private double getTaxFreeAllowance() {return 11_000.00;}
+
+  private double getTaxableIncome(double annualSalary) {return annualSalary - getTaxFreeAllowance();}
+
+  private double getTaxPayable(double annualSalary) {return (annualSalary - getTaxFreeAllowance()) * 0.20;}
+
+  private double getNetPayable(double annualSalary) {return annualSalary - (getNationInsuranceContributions(annualSalary) + getTaxPayable(annualSalary));}
 
   private String formatAmount(double amount) {
     return format("£%.2f", amount);
