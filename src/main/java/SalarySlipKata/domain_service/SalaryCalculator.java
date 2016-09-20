c3bda42240939cbab000d0b28da9317a86beb26b
Mani Sarkar <mani@codurance.com>
2016-09-20 15:41:16 +0100

    Extracted constants in the SalaryCalculator and grouped them by NI contribution and Tax calculation methods.

--- src/main/java/SalarySlipKata/domain_service/SalaryCalculator.java
+++ src/main/java/SalarySlipKata/domain_service/SalaryCalculator.java
@@ -2,13 +2,19 @@ package SalarySlipKata.domain_service;
 
 public class SalaryCalculator {
 
-  public final double getNationInsuranceContributions(double annualSalary) {return (annualSalary - 8060) * 0.12;}
+  private static final double UPTO_AMOUNT_NI_CONTRIBUTION_IS_EXEMPTED = 8_060.00;
+  private static final double NI_BASIC_CONTRIBUTIONS_RATE = 0.12;
 
-  public final double getTaxFreeAllowance() {return 11_000.00;}
+  private static final double PERSONAL_ALLOWANCE = 11_000.00;
+  private static final double TAX_BASIC_RATE = 0.20;
+
+  public final double getNationInsuranceContributions(double annualSalary) {return (annualSalary - UPTO_AMOUNT_NI_CONTRIBUTION_IS_EXEMPTED) * NI_BASIC_CONTRIBUTIONS_RATE;}
+
+  public final double getTaxFreeAllowance() {return PERSONAL_ALLOWANCE;}
 
   public final double getTaxableIncome(double annualSalary) {return annualSalary - getTaxFreeAllowance();}
 
-  public final double getTaxPayable(double annualSalary) {return (annualSalary - getTaxFreeAllowance()) * 0.20;}
+  public final double getTaxPayable(double annualSalary) {return (annualSalary - getTaxFreeAllowance()) * TAX_BASIC_RATE;}
 
   public final double getNetPayable(double annualSalary) {return annualSalary - (getNationInsuranceContributions(annualSalary) + getTaxPayable(annualSalary));}
 }
