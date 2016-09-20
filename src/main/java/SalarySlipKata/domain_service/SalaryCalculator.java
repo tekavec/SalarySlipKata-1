a90c2321e20d786153cad33a2b05040724a5b88f
Mani Sarkar <mani@codurance.com>
2016-09-20 15:44:42 +0100

    Give the constant a better name to hold NI contributions minimum amount after which NI contribution is expected

--- src/main/java/SalarySlipKata/domain_service/SalaryCalculator.java
+++ src/main/java/SalarySlipKata/domain_service/SalaryCalculator.java
@@ -2,13 +2,13 @@ package SalarySlipKata.domain_service;
 
 public class SalaryCalculator {
 
-  private static final double UPTO_AMOUNT_NI_CONTRIBUTION_IS_EXEMPTED = 8_060.00;
+  private static final double AMOUNT_UP_TO_WHICH_NI_CONTRIBUTION_IS_EXEMPTED = 8_060.00;
   private static final double NI_BASIC_CONTRIBUTIONS_RATE = 0.12;
 
   private static final double PERSONAL_ALLOWANCE = 11_000.00;
   private static final double TAX_BASIC_RATE = 0.20;
 
-  public final double getNationInsuranceContributions(double annualSalary) {return (annualSalary - UPTO_AMOUNT_NI_CONTRIBUTION_IS_EXEMPTED) * NI_BASIC_CONTRIBUTIONS_RATE;}
+  public final double getNationInsuranceContributions(double annualSalary) {return (annualSalary - AMOUNT_UP_TO_WHICH_NI_CONTRIBUTION_IS_EXEMPTED) * NI_BASIC_CONTRIBUTIONS_RATE;}
 
   public final double getTaxFreeAllowance() {return PERSONAL_ALLOWANCE;}
 
