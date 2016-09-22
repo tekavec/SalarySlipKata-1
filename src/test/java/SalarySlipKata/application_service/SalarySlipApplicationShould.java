5087590382d37f98a317ac76f653a52de438ea73
Mani Sarkar <mani@codurance.com>
2016-09-22 12:31:19 +0100

    Added clarity to the setup of the tests in SalarySlipApplicationShould, replaced constants with methods with meaningful names returning the same value as the constant, making the setup more readable and helping the verbosity that already exists

--- src/test/java/SalarySlipKata/application_service/SalarySlipApplicationShould.java
+++ src/test/java/SalarySlipKata/application_service/SalarySlipApplicationShould.java
@@ -23,10 +23,10 @@ public class SalarySlipApplicationShould {
 
   private SalarySlipApplication salarySlipApplication;
 
-  private Money annualSalary;
+  private double annualSalary;
   private SalarySlip salarySlip;
 
-  public SalarySlipApplicationShould(Money annualSalary, SalarySlip salarySlip) {
+  public SalarySlipApplicationShould(double annualSalary, SalarySlip salarySlip) {
     this.annualSalary = annualSalary;
     this.salarySlip = salarySlip;
   }
@@ -43,130 +43,130 @@ public class SalarySlipApplicationShould {
     return asList(
         new Object[][] {
             {
-                annualSalaryOf(new Money(24_000.00)),
+                annualSalaryOf(24_000.00),
                 createSalarySlip(
-                    employeeWith(new Money(24_000.00)),
-                    grossSalaryOf(new Money(2000.00)),
-                    taxDetailsWith(taxFreeAllowance(new Money(916.67)), taxableIncome(new Money(1083.33)), taxPayable(new Money(216.67))),
-                    nationalInsuranceContributionsOf(new Money(159.40)),
-                    netPayableOf(new Money(1623.93))
+                    employeeWith(24_000.00),
+                    grossSalaryOf(2000.00),
+                    taxDetailsWith(taxFreeAllowance(916.67), taxableIncome(1083.33), taxPayable(216.67)),
+                    nationalInsuranceContributionsOf(159.40),
+                    netPayableOf(1623.93)
                 )
             },
             {
-                annualSalaryOf(new Money(5_000.00)),
+                annualSalaryOf(5_000.00),
                 createSalarySlip(
-                    employeeWith(new Money(5_000.00)),
-                    grossSalaryOf(new Money(416.67)),
-                    taxDetailsWith(taxFreeAllowance(new Money(916.67)), taxableIncome(new Money(0.00)), taxPayable(new Money(0.00))),
-                    nationalInsuranceContributionsOf(new Money(0.00)),
-                    netPayableOf(new Money(416.67))
+                    employeeWith(5_000.00),
+                    grossSalaryOf(416.67),
+                    taxDetailsWith(taxFreeAllowance(916.67), taxableIncome(0.00), taxPayable(0.00)),
+                    nationalInsuranceContributionsOf(0.00),
+                    netPayableOf(416.67)
                 )
             },
             {
-                annualSalaryOf(new Money(8_060.00)),
+                annualSalaryOf(8_060.00),
                 createSalarySlip(
-                    employeeWith(new Money(8_060.00)),
-                    grossSalaryOf(new Money(671.67)),
-                    taxDetailsWith(taxFreeAllowance(new Money(916.67)), taxableIncome(new Money(0.00)), taxPayable(new Money(0.00))),
-                    nationalInsuranceContributionsOf(new Money(0.00)),
-                    netPayableOf(new Money(671.67))
+                    employeeWith(8_060.00),
+                    grossSalaryOf(671.67),
+                    taxDetailsWith(taxFreeAllowance(916.67), taxableIncome(0.00), taxPayable(0.00)),
+                    nationalInsuranceContributionsOf(0.00),
+                    netPayableOf(671.67)
                 )
             },
             {
-                annualSalaryOf(new Money(12_000.00)),
+                annualSalaryOf(12_000.00),
                 createSalarySlip(
-                    employeeWith(new Money(12_000.00)),
-                    grossSalaryOf(new Money(1000.00)),
-                    taxDetailsWith(taxFreeAllowance(new Money(916.67)), taxableIncome(new Money(83.33)), taxPayable(new Money(16.67))),
-                    nationalInsuranceContributionsOf(new Money(39.40)),
-                    netPayableOf(new Money(943.93))
+                    employeeWith(12_000.00),
+                    grossSalaryOf(1000.00),
+                    taxDetailsWith(taxFreeAllowance(916.67), taxableIncome(83.33), taxPayable(16.67)),
+                    nationalInsuranceContributionsOf(39.40),
+                    netPayableOf(943.93)
                 )
             },
             {
-                annualSalaryOf(new Money(40_000.00)),
+                annualSalaryOf(40_000.00),
                 createSalarySlip(
-                    employeeWith(new Money(40_000.00)),
-                    grossSalaryOf(new Money(3333.33)),
-                    taxDetailsWith(taxFreeAllowance(new Money(916.67)), taxableIncome(new Money(2416.67)), taxPayable(new Money(483.33))),
-                    nationalInsuranceContributionsOf(new Money(319.40)),
-                    netPayableOf(new Money(2530.60))
+                    employeeWith(40_000.00),
+                    grossSalaryOf(3333.33),
+                    taxDetailsWith(taxFreeAllowance(916.67), taxableIncome(2416.67), taxPayable(483.33)),
+                    nationalInsuranceContributionsOf(319.40),
+                    netPayableOf(2530.60)
                 )
             },
             {
-                annualSalaryOf(new Money(43_000.00)),
+                annualSalaryOf(43_000.00),
                 createSalarySlip(
-                    employeeWith(new Money(43_000.00)),
-                    grossSalaryOf(new Money(3583.33)),
-                    taxDetailsWith(taxFreeAllowance(new Money(916.67)), taxableIncome(new Money(2666.67)), taxPayable(new Money(533.33))),
-                    nationalInsuranceContributionsOf(new Money(349.40)),
-                    netPayableOf(new Money(2700.60))
+                    employeeWith(43_000.00),
+                    grossSalaryOf(3583.33),
+                    taxDetailsWith(taxFreeAllowance(916.67), taxableIncome(2666.67), taxPayable(533.33)),
+                    nationalInsuranceContributionsOf(349.40),
+                    netPayableOf(2700.60)
                 )
             },
             {
-                annualSalaryOf(new Money(60_000.00)),
+                annualSalaryOf(60_000.00),
                 createSalarySlip(
-                    employeeWith(new Money(60_000.00)),
-                    grossSalaryOf(new Money(5000.00)),
-                    taxDetailsWith(taxFreeAllowance(new Money(916.67)), taxableIncome(new Money(4083.33)), taxPayable(new Money(1100.00))),
-                    nationalInsuranceContributionsOf(new Money(377.73)),
-                    netPayableOf(new Money(3522.27))
+                    employeeWith(60_000.00),
+                    grossSalaryOf(5000.00),
+                    taxDetailsWith(taxFreeAllowance(916.67), taxableIncome(4083.33), taxPayable(1100.00)),
+                    nationalInsuranceContributionsOf(377.73),
+                    netPayableOf(3522.27)
                 )
             },
             {
-                annualSalaryOf(new Money(100_000.00)),
+                annualSalaryOf(100_000.00),
                 createSalarySlip(
-                    employeeWith(new Money(100_000.00)),
-                    grossSalaryOf(new Money(8333.33)),
-                    taxDetailsWith(taxFreeAllowance(new Money(916.67)), taxableIncome(new Money(7416.67)), taxPayable(new Money(2433.33))),
-                    nationalInsuranceContributionsOf(new Money(444.40)),
-                    netPayableOf(new Money(5455.60))
+                    employeeWith(100_000.00),
+                    grossSalaryOf(8333.33),
+                    taxDetailsWith(taxFreeAllowance(916.67), taxableIncome(7416.67), taxPayable(2433.33)),
+                    nationalInsuranceContributionsOf(444.40),
+                    netPayableOf(5455.60)
                 )
             },
             {
-                annualSalaryOf(new Money(111_000.00)),
+                annualSalaryOf(111_000.00),
                 createSalarySlip(
-                    employeeWith(new Money(111_000.00)),
-                    grossSalaryOf(new Money(9250.00)),
-                    taxDetailsWith(taxFreeAllowance(new Money(458.33)), taxableIncome(new Money(8791.67)), taxPayable(new Money(2983.33))),
-                    nationalInsuranceContributionsOf(new Money(462.73)),
-                    netPayableOf(new Money(5803.94))
+                    employeeWith(111_000.00),
+                    grossSalaryOf(9250.00),
+                    taxDetailsWith(taxFreeAllowance(458.33), taxableIncome(8791.67), taxPayable(2983.33)),
+                    nationalInsuranceContributionsOf(462.73),
+                    netPayableOf(5803.94)
                 )
             },
             {
-                annualSalaryOf(new Money(122_000.00)),
+                annualSalaryOf(122_000.00),
                 createSalarySlip(
-                    employeeWith(new Money(122_000.00)),
-                    grossSalaryOf(new Money(10166.67)),
-                    taxDetailsWith(taxFreeAllowance(new Money(0.00)), taxableIncome(new Money(10166.67)), taxPayable(new Money(3533.33))),
-                    nationalInsuranceContributionsOf(new Money(481.07)),
-                    netPayableOf(new Money(6152.27))
+                    employeeWith(122_000.00),
+                    grossSalaryOf(10166.67),
+                    taxDetailsWith(taxFreeAllowance(0.00), taxableIncome(10166.67), taxPayable(3533.33)),
+                    nationalInsuranceContributionsOf(481.07),
+                    netPayableOf(6152.27)
                 )
             },
             {
-                annualSalaryOf(new Money(150_000.00)),
+                annualSalaryOf(150_000.00),
                 createSalarySlip(
-                    employeeWith(new Money(150_000.00)),
-                    grossSalaryOf(new Money(12500.00)),
-                    taxDetailsWith(taxFreeAllowance(new Money(0.00)), taxableIncome(new Money(12500.00)), taxPayable(new Money(4466.67))),
-                    nationalInsuranceContributionsOf(new Money(527.73)),
-                    netPayableOf(new Money(7505.60))
+                    employeeWith(150_000.00),
+                    grossSalaryOf(12500.00),
+                    taxDetailsWith(taxFreeAllowance(0.00), taxableIncome(12500.00), taxPayable(4466.67)),
+                    nationalInsuranceContributionsOf(527.73),
+                    netPayableOf(7505.60)
                 )
             },
             {
-                annualSalaryOf(new Money(160_000.00)),
+                annualSalaryOf(160_000.00),
                 createSalarySlip(
-                    employeeWith(new Money(160_000.00)),
-                    grossSalaryOf(new Money(13333.33)),
-                    taxDetailsWith(taxFreeAllowance(new Money(0.00)), taxableIncome(new Money(13333.33)), taxPayable(new Money(4841.67))),
-                    nationalInsuranceContributionsOf(new Money(544.40)),
-                    netPayableOf(new Money(7947.26))
+                    employeeWith(160_000.00),
+                    grossSalaryOf(13333.33),
+                    taxDetailsWith(taxFreeAllowance(0.00), taxableIncome(13333.33), taxPayable(4841.67)),
+                    nationalInsuranceContributionsOf(544.40),
+                    netPayableOf(7947.26)
                 )
             }
         }
     );
   }
 
-  private static Money annualSalaryOf(Money money) {
+  private static double annualSalaryOf(double money) {
     return money;
   }
 
@@ -177,36 +177,36 @@ public class SalarySlipApplicationShould {
     );
   }
 
-  private static Employee employeeWith(Money annualSalary) {
-    return new Employee(12345, "John J Doe", annualSalary);
+  private static Employee employeeWith(double annualSalary) {
+    return new Employee(12345, "John J Doe", new Money(annualSalary));
   }
 
-  private static Money grossSalaryOf(Money money) {
-    return money;
+  private static Money grossSalaryOf(double money) {
+    return new Money(money);
   }
 
-  private static Money nationalInsuranceContributionsOf(Money money) {
-    return money;
+  private static Money nationalInsuranceContributionsOf(double money) {
+    return new Money(money);
   }
 
   private static TaxDetails taxDetailsWith(Money taxFreeAllowance, Money taxableIncome, Money taxPayable) {
     return new TaxDetails(taxFreeAllowance, taxableIncome, taxPayable);
   }
 
-  private static Money taxFreeAllowance(Money money) {
-    return money;
+  private static Money taxFreeAllowance(double money) {
+    return new Money(money);
   }
 
-  private static Money taxableIncome(Money money) {
-    return money;
+  private static Money taxableIncome(double money) {
+    return new Money(money);
   }
 
-  private static Money taxPayable(Money money) {
-    return money;
+  private static Money taxPayable(double money) {
+    return new Money(money);
   }
 
-  private static Money netPayableOf(Money money) {
-    return money;
+  private static Money netPayableOf(double money) {
+    return new Money(money);
   }
 
   @Test public void
