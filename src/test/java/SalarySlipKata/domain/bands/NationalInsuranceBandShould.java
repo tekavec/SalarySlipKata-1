1765cba5d4472bcbb45bf8f88f7f3f984d110a94
Mani Sarkar <mani@codurance.com>
2016-09-29 11:05:37 +0100

    Extracted the test cases for NationalInsuranceBand from the BandShould test class

--- src/test/java/SalarySlipKata/domain/bands/NationalInsuranceBandShould.java
+++ src/test/java/SalarySlipKata/domain/bands/NationalInsuranceBandShould.java
@@ -23,46 +23,47 @@ public class NationalInsuranceBandShould {
 
   private final Money annualSalary;
   private final NationalInsuranceBand nationalInsuranceBand;
-
-  private final Money expectedNIContributionsForABand;
+  private final Money expectedNIContributionsForTheBand;
 
   @Parameterized.Parameters(name = "For an annual salary of {0}, the NI Contribution is {2} for the band {1}")
   public static Collection<Object[]> data() {
     return asList(
         new Object[][] {
-            { annualSalaryOf( 5_000.00), NO_CONTRIBUTIONS_BAND,     expectedNIContributions(            zero()) },
-            { annualSalaryOf( 8_060.00), NO_CONTRIBUTIONS_BAND,     expectedNIContributions(            zero()) },
-            { annualSalaryOf(10_000.00), NO_CONTRIBUTIONS_BAND,     expectedNIContributions(            zero()) },
-            { annualSalaryOf(10_000.00), BASIC_CONTRIBUTIONS_BAND,  expectedNIContributions( new Money(232.80)) },
-            { annualSalaryOf(40_000.00), BASIC_CONTRIBUTIONS_BAND,  expectedNIContributions(new Money(3832.80)) },
-            { annualSalaryOf(43_000.00), NO_CONTRIBUTIONS_BAND,     expectedNIContributions(            zero()) },
-            { annualSalaryOf(43_000.00), BASIC_CONTRIBUTIONS_BAND,  expectedNIContributions(new Money(4192.80)) },
-            { annualSalaryOf(45_000.00), BASIC_CONTRIBUTIONS_BAND,  expectedNIContributions(new Money(4192.80)) },
-            { annualSalaryOf(45_000.00), HIGHER_CONTRIBUTIONS_BAND, expectedNIContributions(  new Money(40.00)) },
-            { annualSalaryOf(50_000.00), NO_CONTRIBUTIONS_BAND,     expectedNIContributions(            zero()) },
-            { annualSalaryOf(50_000.00), BASIC_CONTRIBUTIONS_BAND,  expectedNIContributions(new Money(4192.80)) },
-            { annualSalaryOf(50_000.00), HIGHER_CONTRIBUTIONS_BAND, expectedNIContributions( new Money(140.00)) },
+            { annualSalaryOf( 5_000.00), NO_CONTRIBUTIONS_BAND,     expectedNIContributionsOf(   0.00) },
+            { annualSalaryOf( 8_060.00), NO_CONTRIBUTIONS_BAND,     expectedNIContributionsOf(   0.00) },
+            { annualSalaryOf(10_000.00), NO_CONTRIBUTIONS_BAND,     expectedNIContributionsOf(   0.00) },
+            { annualSalaryOf(10_000.00), BASIC_CONTRIBUTIONS_BAND,  expectedNIContributionsOf( 232.80) },
+            { annualSalaryOf(40_000.00), BASIC_CONTRIBUTIONS_BAND,  expectedNIContributionsOf(3832.80) },
+            { annualSalaryOf(43_000.00), NO_CONTRIBUTIONS_BAND,     expectedNIContributionsOf(   0.00) },
+            { annualSalaryOf(43_000.00), BASIC_CONTRIBUTIONS_BAND,  expectedNIContributionsOf(4192.80) },
+            { annualSalaryOf(45_000.00), BASIC_CONTRIBUTIONS_BAND,  expectedNIContributionsOf(4192.80) },
+            { annualSalaryOf(45_000.00), HIGHER_CONTRIBUTIONS_BAND, expectedNIContributionsOf(  40.00) },
+            { annualSalaryOf(50_000.00), NO_CONTRIBUTIONS_BAND,     expectedNIContributionsOf(   0.00) },
+            { annualSalaryOf(50_000.00), BASIC_CONTRIBUTIONS_BAND,  expectedNIContributionsOf(4192.80) },
+            { annualSalaryOf(50_000.00), HIGHER_CONTRIBUTIONS_BAND, expectedNIContributionsOf( 140.00) },
         }
     );
   }
 
-  private static Money annualSalaryOf(double denomination) {return new Money(denomination);}
+  private static Money annualSalaryOf(double amount) {return new Money(amount);}
 
-  private static Money expectedNIContributions(Money amount) {
-    return amount;
+  private static Money expectedNIContributionsOf(double amount) {
+    return new Money(amount);
   }
 
   public NationalInsuranceBandShould(
-      final Money annualSalary, final NationalInsuranceBand nationalInsuranceBand, final Money expectedNIContributionsForABand) {
+      final Money annualSalary,
+      final NationalInsuranceBand nationalInsuranceBand,
+      final Money expectedNIContributionsForTheBand) {
     this.annualSalary = annualSalary;
     this.nationalInsuranceBand = nationalInsuranceBand;
-    this.expectedNIContributionsForABand = expectedNIContributionsForABand;
+    this.expectedNIContributionsForTheBand = expectedNIContributionsForTheBand;
   }
 
   @Test
   public void
-  return_the_NI_contributions_for_a_given_range_and_rate_for_a_given_annual_salary() {
+  return_the_NI_contributions_for_a_given_NI_contribution_band_for_a_given_annual_salary() {
     final Money actualAnnualNIContributions = nationalInsuranceBand.calculateFrom(annualSalary);
-    assertThat(actualAnnualNIContributions, is(expectedNIContributionsForABand));
+    assertThat(actualAnnualNIContributions, is(expectedNIContributionsForTheBand));
   }
 }
