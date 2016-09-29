6a371a383139e9ee30c85e7886496638c91cfcf1
Mani Sarkar <mani@codurance.com>
2016-09-29 11:26:43 +0100

    Changed the type of the parameter from using Band to using TaxBand which is more specific to the HigherTaxWithPersonalAllowanceReductionRuleBand

--- src/main/java/SalarySlipKata/domain/bands/HigherTaxWithPersonalAllowanceReductionRuleBand.java
+++ src/main/java/SalarySlipKata/domain/bands/HigherTaxWithPersonalAllowanceReductionRuleBand.java
@@ -6,31 +6,31 @@ import SalarySlipKata.domain.Money;
 import SalarySlipKata.domain_service.PersonalAllowanceCalculator;
 
 public class HigherTaxWithPersonalAllowanceReductionRuleBand implements Band {
-  private Band band;
+  private TaxBand taxBand;
   private PersonalAllowanceCalculator personalAllowanceCalculator;
 
   public HigherTaxWithPersonalAllowanceReductionRuleBand(
-      Band band, PersonalAllowanceCalculator personalAllowanceCalculator) {
-    this.band = band;
+      TaxBand taxBand, PersonalAllowanceCalculator personalAllowanceCalculator) {
+    this.taxBand = taxBand;
     this.personalAllowanceCalculator = personalAllowanceCalculator;
   }
 
   @Override
   public Money calculateFrom(Money annualSalary) {
     Money personalAllowanceAdjustmentForOver100K = zero();
-    if (personalAllowanceCalculator.reductionRuleAppliesBetween(band.lowerLimit(), band.upperLimit())) {
+    if (personalAllowanceCalculator.reductionRuleAppliesBetween(taxBand.lowerLimit(), taxBand.upperLimit())) {
       personalAllowanceAdjustmentForOver100K =
           personalAllowanceCalculator.calculateAdjustmentForExcessOver100K(annualSalary);
     }
 
     if (annualSalary.isBetweenAndInclusiveOf(lowerLimit(), upperLimit())) {
-      final Money excessIncome = band.calculateExcessFrom(annualSalary, band.lowerLimit());
-      return excessIncome.plus(personalAllowanceAdjustmentForOver100K).multiplyBy(band.rate());
+      final Money excessIncome = taxBand.calculateExcessFrom(annualSalary, taxBand.lowerLimit());
+      return excessIncome.plus(personalAllowanceAdjustmentForOver100K).multiplyBy(taxBand.rate());
     }
 
     if (annualSalary.isGreaterThan(upperLimit())) {
-      final Money excessIncome = band.calculateExcessFrom(band.upperLimit(), band.lowerLimit());
-      return excessIncome.plus(personalAllowanceAdjustmentForOver100K).multiplyBy(band.rate());
+      final Money excessIncome = taxBand.calculateExcessFrom(taxBand.upperLimit(), taxBand.lowerLimit());
+      return excessIncome.plus(personalAllowanceAdjustmentForOver100K).multiplyBy(taxBand.rate());
     }
 
     return zero();
@@ -38,21 +38,21 @@ public class HigherTaxWithPersonalAllowanceReductionRuleBand implements Band {
 
   @Override
   public Money calculateExcessFrom(Money upperLimit, Money lowerLimit) {
-    return band.calculateExcessFrom(upperLimit, lowerLimit);
+    return taxBand.calculateExcessFrom(upperLimit, lowerLimit);
   }
 
   @Override
   public Money lowerLimit() {
-    return band.lowerLimit();
+    return taxBand.lowerLimit();
   }
 
   @Override
   public Money upperLimit() {
-    return band.upperLimit();
+    return taxBand.upperLimit();
   }
 
   @Override
   public double rate() {
-    return band.rate();
+    return taxBand.rate();
   }
 }
