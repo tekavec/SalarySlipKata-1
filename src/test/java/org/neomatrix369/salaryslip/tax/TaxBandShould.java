492cf0af4b7f8c68c63c2b3c29b0e827e30c0c0c
Mani Sarkar <mani@codurance.com>
2016-09-30 16:40:59 +0100

    Renamed methods and fields in the TaxBandShould class to reflect the data they represent.

--- src/test/java/org/neomatrix369/salaryslip/tax/TaxBandShould.java
+++ src/test/java/org/neomatrix369/salaryslip/tax/TaxBandShould.java
@@ -26,30 +26,30 @@ public class TaxBandShould {
 
   private final Money annualSalary;
   private final TaxBand taxBand;
-  private final Money expectedTaxPayableForTheBand;
+  private final Money expectedAnnualTaxPayableForTheBand;
 
-  @Parameterized.Parameters(name = "For an annual salary of {0}, the Tax payable is {2} for the band {1}")
+  @Parameterized.Parameters(name = "For an annual salary of {0}, the annual tax payable is {2} for the band {1}")
   public static Collection<Object[]> data() {
     return asList(
         new Object[][] {
-            { annualSalaryOf( 60_000.00), ZERO_TAX_BAND,       expectedTaxPayableOf(     0.00) },
-            { annualSalaryOf( 60_000.00), BASIC_TAX_BAND,      expectedTaxPayableOf( 6_400.00) },
-            { annualSalaryOf( 60_000.00), HIGHER_TAX_BAND,     expectedTaxPayableOf( 6_800.00) },
-            { annualSalaryOf( 60_000.00), ADDITIONAL_TAX_BAND, expectedTaxPayableOf(     0.00) },
-            { annualSalaryOf(100_000.00), ZERO_TAX_BAND,       expectedTaxPayableOf(     0.00) },
-            { annualSalaryOf(100_000.00), BASIC_TAX_BAND,      expectedTaxPayableOf( 6_400.00) },
-            { annualSalaryOf(100_000.00), HIGHER_TAX_BAND,     expectedTaxPayableOf(22_800.00) },
-            { annualSalaryOf(100_000.00), ADDITIONAL_TAX_BAND, expectedTaxPayableOf(     0.00) },
+            { annualSalaryOf( 60_000.00), ZERO_TAX_BAND,       expectedAnnualTaxPayableOf(     0.00) },
+            { annualSalaryOf( 60_000.00), BASIC_TAX_BAND,      expectedAnnualTaxPayableOf( 6_400.00) },
+            { annualSalaryOf( 60_000.00), HIGHER_TAX_BAND,     expectedAnnualTaxPayableOf( 6_800.00) },
+            { annualSalaryOf( 60_000.00), ADDITIONAL_TAX_BAND, expectedAnnualTaxPayableOf(     0.00) },
+            { annualSalaryOf(100_000.00), ZERO_TAX_BAND,       expectedAnnualTaxPayableOf(     0.00) },
+            { annualSalaryOf(100_000.00), BASIC_TAX_BAND,      expectedAnnualTaxPayableOf( 6_400.00) },
+            { annualSalaryOf(100_000.00), HIGHER_TAX_BAND,     expectedAnnualTaxPayableOf(22_800.00) },
+            { annualSalaryOf(100_000.00), ADDITIONAL_TAX_BAND, expectedAnnualTaxPayableOf(     0.00) },
             { annualSalaryOf(105_000.00), HIGHER_TAX_WITH_PA_REDUCTION_RULE_BAND,
-                                                               expectedTaxPayableOf(25_800.00) },
+                                                               expectedAnnualTaxPayableOf(25_800.00) },
             { annualSalaryOf(111_000.00), HIGHER_TAX_WITH_PA_REDUCTION_RULE_BAND,
-                                                               expectedTaxPayableOf(29_400.00) },
+                                                               expectedAnnualTaxPayableOf(29_400.00) },
             { annualSalaryOf(122_000.00), HIGHER_TAX_WITH_PA_REDUCTION_RULE_BAND,
-                                                               expectedTaxPayableOf(36_000.00) },
+                                                               expectedAnnualTaxPayableOf(36_000.00) },
             { annualSalaryOf(130_000.00), HIGHER_TAX_WITH_PA_REDUCTION_RULE_BAND,
-                                                               expectedTaxPayableOf(39_200.00) },
+                                                               expectedAnnualTaxPayableOf(39_200.00) },
             { annualSalaryOf(150_000.00), HIGHER_TAX_WITH_PA_REDUCTION_RULE_BAND,
-                                                               expectedTaxPayableOf(47_200.00) },
+                                                               expectedAnnualTaxPayableOf(47_200.00) },
 
         }
     );
@@ -57,23 +57,23 @@ public class TaxBandShould {
 
   private static Money annualSalaryOf(double amount) {return new Money(amount);}
 
-  private static Money expectedTaxPayableOf(double amount) {
+  private static Money expectedAnnualTaxPayableOf(double amount) {
     return new Money(amount);
   }
 
   public TaxBandShould(
       final Money annualSalary,
       final TaxBand taxBand,
-      final Money expectedTaxPayableForTheBand) {
+      final Money expectedAnnualTaxPayableForTheBand) {
     this.annualSalary = annualSalary;
     this.taxBand = taxBand;
-    this.expectedTaxPayableForTheBand = expectedTaxPayableForTheBand;
+    this.expectedAnnualTaxPayableForTheBand = expectedAnnualTaxPayableForTheBand;
   }
 
   @Test
   public void
   return_the_Tax_Payable_for_a_given_tax_band_for_a_given_annual_salary() {
     final Money actualAnnualTaxPayable = taxBand.calculateTaxFrom(annualSalary);
-    assertThat(actualAnnualTaxPayable, is(expectedTaxPayableForTheBand));
+    assertThat(actualAnnualTaxPayable, is(expectedAnnualTaxPayableForTheBand));
   }
 }
